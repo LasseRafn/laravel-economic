f86b2867bf499177b9ea6f70dfa6da28a9438eaf
Lasse Rafn <lasserafn@gmail.com>
2016-12-05 14:44:24 +0100

    Removed lol() function. sorry

--- src/Utils/Request.php
+++ src/Utils/Request.php
@@ -18,20 +18,4 @@ class Request
 		    ]
 		] );
 	}
-
-
-	function lol()
-	{
-		// fixme!
-		try
-		{
-			$url      = config( 'pipedrive.endpoint' ) . $this->buildEntity( $entity, $id, $fields ) . '?api_token=' . $this-$this->api_token . '&start=' . $start . '&limit=' . $limit;
-			$response = $this->curl->get( $url );
-
-			return $this->getData( $response->getBody() );
-		} catch ( \Exception $exception )
-		{
-			throw new \Exception( $exception->getMessage(), $exception->getCode() );
-		}
-	}
 }

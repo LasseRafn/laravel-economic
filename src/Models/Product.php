5c3a980c2f5e262672aa3aca5e1cd830675e6e8c
Lasse Rafn <lasserafn@gmail.com>
2017-01-16 14:00:32 +0100

    Added variables to product

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -4,16 +4,25 @@ use LasseRafn\Economic\Utils\Model;
 
 class Product extends Model
 {
-	protected $entity   = 'products';
+	protected $entity     = 'products';
 	protected $primaryKey = 'productNumber';
-	protected $fillable = [
+	protected $fillable   = [
 		'productNumber',
-	    'name',
-	    'self',
-	    'unit',
-	    'description',
-	    'recommendedPrice',
-	    'salesPrice',
-	    'costPrice'
+		'name',
+		'self',
+		'unit',
+		'description',
+		'recommendedPrice',
+		'salesPrice',
+		'costPrice'
 	];
+
+	public $productNumber;
+	public $name;
+	public $self;
+	public $unit;
+	public $description;
+	public $recommendedPrice;
+	public $salesPrice;
+	public $costPrice;
 }

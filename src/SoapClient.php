5b3740b19e81647a97b21b44c72bf93ca07fdcfc
Stefan Ninic <ninicstefan94@gmail.com>
2018-04-20 21:05:25 +0200

    Fixed method name

--- src/SoapClient.php
+++ src/SoapClient.php
@@ -131,7 +131,7 @@ class SoapClient
         return $entries;
     }
 
-    public function createCahBookEntryFromData( $data )
+    public function createCashBookEntryFromData( $data )
     {
         return $this->soap->call( 'economic.CashBookEntry_CreateFromData', [
             'CashBookEntry_CreateFromData' => [
